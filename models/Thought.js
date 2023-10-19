9df7a86df502cd4ee22c15403b268efba28636e4
Charles Chavis <chavisca@gmail.com>
2023-10-19 02:20:08 -0400

    moved reactionSchema ahead of thoughtSchema due to order of import conflict

--- models/Thought.js
+++ models/Thought.js
@@ -1,33 +1,5 @@
 const { Schema, model } = require('mongoose');
 
-// Schema to create a Thought model
-const thoughtSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: true,
-      minlength: 1,
-      maxlength: 280,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-      },
-    username: {
-    type: String,
-    required: true,
-    },
-    reactions: [{ reactionSchema }],
-}, 
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true,
-    },
-    id: false,
-  }
-);
-
 const reactionSchema = new Schema(
   {
     reactionId: {
@@ -51,6 +23,34 @@ const reactionSchema = new Schema(
   },
   {
     toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Schema to create a Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    username: {
+    type: String,
+    required: true,
+    },
+    reactions: [{ reactionSchema }],
+}, 
+  {
+    toJSON: {
+      virtuals: true,
       getters: true,
     },
     id: false,
