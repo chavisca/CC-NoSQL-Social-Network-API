60f08b4189159e40aa1b074a522562ad6e1795a1
Charles Chavis <chavisca@gmail.com>
2023-10-20 23:15:08 -0400

    removed heroku connection portion of config, replaced with direct connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,5 @@
 const { connect, connection } = require('mongoose');
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socnetDB';
 
-connect(connectionString);
+connect('mongodb://127.0.0.1:27017/socnetDB');
 
 module.exports = connection;
